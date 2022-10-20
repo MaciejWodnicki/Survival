bcdf993d117df0e905e748d5d1cd0e4c46bd4368
Bieniecki-Lukasz <lukasz.bieniecki@edu.uni.lodz.pl>
2022-10-20 23:59:39 +0200

    Test

--- Source/Survival/SurvivalGameModeBase.h
+++ Source/Survival/SurvivalGameModeBase.h
@@ -1,14 +1,9 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "GameFramework/GameModeBase.h"
 #include "SurvivalGameModeBase.generated.h"
 
-/**
- * 
- */
 UCLASS()
 class SURVIVAL_API ASurvivalGameModeBase : public AGameModeBase
 {
