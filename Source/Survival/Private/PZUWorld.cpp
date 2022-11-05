56fbe052eb3c730258e7f4a3be0a74ffb1830176
Mateusz-Papis <mateuszpapis97@gmail.com>
2022-11-05 15:39:44 +0100

    test

--- Source/Survival/Private/PZUWorld.cpp
+++ Source/Survival/Private/PZUWorld.cpp
@@ -1,5 +1,27 @@
 #include "PZUWorld.h"
 
+void AddTree(FVector location, FPZUMap& map)
+{
+	for (int x = -2;x < 3;x++)
+		for (int y = -2;y < 3;y++)
+		{
+			map.blocks.Add(location + FVector(x, y, 3), 4);
+			map.blocks.Add(location + FVector(x, y, 4), 4);
+		}
+	for (int x = -1;x < 2;x++)
+		for (int y = -1;y < 2;y++)
+		{
+			map.blocks.Add(location + FVector(x, y, 5), 4);
+		}
+
+	map.blocks.Add(location, 3);
+	map.blocks.Add(location + FVector(0, 0, 1), 3);
+	map.blocks.Add(location + FVector(0, 0, 2), 3);
+	map.blocks.Add(location + FVector(0, 0, 3), 3);
+	map.blocks.Add(location + FVector(0, 0, 4), 3);
+	map.blocks.Add(location + FVector(0, 0, 6), 4);
+}
+
 PZUWorld::PZUWorld()
 {
 	blockDefinitions.Add(0, "dirt");
@@ -18,16 +40,79 @@ PZUWorld::PZUWorld()
 	blockDefinitions.Add(12, "sand");
 
 	FPZUMap map;
+	int sizex = 128;
+	int sizey = 128;
+	int sizez = 64;
+	
+	map.size = FVector(sizex, sizey, sizez);
 
-	map.size = FVector(64, 64, 32);
-
-	for (int x = 0;x < 64;x++)
-		for (int y = 0;y < 64;y++)
+	for (int x = 0;x < sizex;x++)
+		for (int y = 0;y < sizey;y++)
 		{
+			map.blocks.Add(FVector(x, y, 0), 1);
+			if (x < 128 && y < 24)
+			{
+				map.blocks.Add(FVector(x, y, 0), 12);
+			}
 
-			//map.blocks.Add(FVector(x, y, 0), 0);
+			
+				/*for (int z = 0; z < 32; z++)
+					if (x == y)
+					{
+						for (int i = 0; i < z; i++)
+						{
+							map.blocks.Add(FVector(x - z - 64, y + z, i), 2);
+							map.blocks.Add(FVector(x - z - 65, y + z, i), 2);
+							map.blocks.Add(FVector(x - z - 66, y + z, i), 2);
+
+						}
+					}*/
+
+				/*for (int z = 0; z < 32; z++)
+					if (sizex - x == y)
+					{
+						for (int i = 0; i < z; i++)
+						{
+							map.blocks.Add(FVector(-(x - z + 64) + sizex, y - z, i), 2);
+							map.blocks.Add(FVector(-(x - z + 65) + sizex, y - z, i), 2);
+							map.blocks.Add(FVector(-(x - z + 66) + sizex, y - z, i), 2);
+
+						}
+					}*/
+			for (int z = 0; z < 32; z++)
+			{
+				if (sizey - y == 100 - z)
+				{	
+					for (int i = 0; i < z; i++)
+					{
+						map.blocks.Add(FVector(x, y + 70, i), 2);
+					}
+				}
+
+			}
+
+
+
+			
+		}
+	
+	
+	for(int x = 0; x < sizex + 1; x++)
+		for (int z = 0; z < sizez + 1; z++)
+		{
+			map.blocks.Remove(FVector(x, -1, z));
+			map.blocks.Remove(FVector(x, sizey, z));
 		}
 
+	for (int y = 0; y < sizey + 1; y++)
+		for (int z = 0; z < sizez + 1; z++)
+		{
+			map.blocks.Remove(FVector(-1, y, z));
+			map.blocks.Remove(FVector(sizex, y, z));
+		}
+
+
+	AddTree(FVector(5, 8, 1), map);
 
 	maps.Add(map);
 	SetActiveMap(0);
