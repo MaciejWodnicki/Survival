543c0883bbdeec9d6d4ca09372088349d0caff72
Maciej Wodnicki <wodnicki.maciej@gmail.com>
2022-12-10 21:55:25 +0100

    added skeletonacter cpp class

---
+++ Source/Survival/PZUSkeletonActor.cpp
@@ -0,0 +1,20 @@
+﻿#include "PZUSkeletonActor.h"
+
+// Ustawia wartości domyślne
+APZUSkeletonActor::APZUSkeletonActor()
+{
+	// Ustaw tę actor, aby wywoływać funkcję Tick() w każdej ramce. Możesz wyłączyć tę funkcję, aby zwiększyć wydajność, jeśli jej nie potrzebujesz.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Wywołuje się, gdy gra się rozpoczyna lub po pojawieniu się
+void APZUSkeletonActor::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+// Wywoływane co klatkę
+void APZUSkeletonActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
